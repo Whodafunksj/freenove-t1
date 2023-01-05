28dd65fdc477ad4c14ceda1977348f973ab4ad9d
Whodafunksj <96833381+Whodafunksj@users.noreply.github.com>
2023-01-05 10:34:06 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+	
+})
+basic.showString("Hello! Welcome to the escape room!!")
+basic.showString("Tap LOGO for 4secs to begin")
+basic.forever(function () {
+	
+})
